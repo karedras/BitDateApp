3f62c911743ff9d1121d1e8d7fc4a0da475d9059
Adriel Carsete <adrielcarsete@mac1733.local>
2015-06-11 19:58:29 -0300

    Initial Commit

---
+++ BitDateApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitDateApp
+//
+//  Created by Adriel Carsete on 6/11/15.
+//  Copyright (c) 2015 Adriel Carsete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
